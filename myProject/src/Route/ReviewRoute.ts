6c38e5cbc8030bab1b5f0664e7d4b3d39e82b2b0
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2023-06-19 13:32:27 +0300

    .

--- myProject/src/Route/ReviewRoute.ts
+++ myProject/src/Route/ReviewRoute.ts
@@ -7,7 +7,7 @@ export const createReviewRoute = async (req: Request, res: Response) => {
     const revAttData = { user, average, relatedType, relatedId , nbReview: null };
     const revAtt = await createReview(revAttData);
 
-    res.status(201).json({ message: 'Review Attraction created successfully', revAtt });
+    res.status(201).json({ message: 'Review created successfully', revAtt });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error', error: error.message });
