ba3be800fd45e87b32122247b25fbac805a4753f
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2023-06-20 13:00:18 +0300

    hello world in vue3

--- frontEnd/trifinderFrontEnd/src/App.vue
+++ frontEnd/trifinderFrontEnd/src/App.vue
@@ -3,12 +3,12 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
+<!-- <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it! mhammad" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -18,7 +18,16 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+</template> -->
+
+<template>
+  <div>
+    <h1>Hello, World!</h1>
+    <h2>Welcome mhammad</h2>
+    
+  </div>
 </template>
+
 
 <style scoped>
 header {
